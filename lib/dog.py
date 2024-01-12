75c4a09acfc71413aa98ff82a1447c8224e12fa4
Dennis Kiboi <dennis.kiboi@moringaschool.com>
2024-01-12 11:13:23 +0300

    Completed Assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,26 @@
 from models import Dog
+from sqlalchemy.orm import sessionmaker
 
-def create_table(base):
-    pass
+def create_table(base, engine):
+    base.metadata.create_all(engine)
+    # Session = sessionmaker(bind=engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter(Dog.name.like(f"%{name}%")).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter(Dog.name.like(f"%{name}%"), Dog.breed.like(f"%{breed}%")).first()
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
